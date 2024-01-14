517bc26dec6eb1fb73f9f70d26caf03989235560
genisida <123615141+genisida@users.noreply.github.com>
2024-01-14 18:41:50 +0900

    Create code.js

---
+++ src/code.js
@@ -0,0 +1,21 @@
+const choose = a => a[Math.floor(Math.random()*a.length)];
+const element = choose([...document.querySelectorAll("#bodyContent a:not(.mw-file-description)")].filter(x=>new URLPattern("https://ja.wikipedia.org/wiki/:title").test(x.href)));
+
+element.classList.add("rainbow");
+const a = new CSSStyleSheet();
+a.replaceSync(`.rainbow {
+    /* 横長で虹色を作る */
+    border: 2px solid #36CF;
+    animation: gaming .5s linear infinite;
+  }
+  @keyframes gaming {
+    0% {border-color:#36CF;}
+    50% {border-color:#36C0;}
+    100% {border-color:#36CF;}
+  }`);
+document.adoptedStyleSheets = [a];
+
+element.scrollIntoView({behavior: "smooth",block: "center",inline:"center",});
+element.focus({preventScroll:true});
+await new Promise(r=>setTimeout(r,1000));
+element.click();
