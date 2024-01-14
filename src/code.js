32c0ecf03ccd62031659bbd8abfa1eb1cf8e9ede
genisida <123615141+genisida@users.noreply.github.com>
2024-01-14 18:45:26 +0900

    Update code.js

--- src/code.js
+++ src/code.js
@@ -1,20 +1,26 @@
 const choose = a => a[Math.floor(Math.random()*a.length)];
+
+//リンクを選ぶ
 const element = choose([...document.querySelectorAll("#bodyContent a:not(.mw-file-description)")].filter(x=>new URLPattern("https://ja.wikipedia.org/wiki/:title").test(x.href)));
 
-element.classList.add("rainbow");
+//リンクを光らせる
+element.classList.add("link");
 const a = new CSSStyleSheet();
-a.replaceSync(`.rainbow {
+a.replaceSync(`.link {
     border: 2px solid #36CF;
-    animation: gaming .5s linear infinite;
+    animation: waving .5s linear infinite;
   }
-  @keyframes gaming {
+  @keyframes waving {
     0% {border-color:#36CF;}
     50% {border-color:#36C0;}
     100% {border-color:#36CF;}
   }`);
 document.adoptedStyleSheets = [a];
 
+//スクロールする
 element.scrollIntoView({behavior: "smooth",block: "center",inline:"center",});
 element.focus({preventScroll:true});
 await new Promise(r=>setTimeout(r,1000));
+
+//リンクに遷移する
 element.click();
